4aceb670ba78ac480580323aa5dd852fe4383d8b
Italo M <italomarcos0010@gmail.com>
2019-12-14 21:00:08 -0300

    stylizing -- done

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -3,7 +3,9 @@ import { Keyboard, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import api from '../../services/api';
 
-import { Container, Form, Input, SubmitButton } from './styles';
+import { Container, Form, Input, SubmitButton, List } from './styles';
+import { User, Avatar, Name, Bio, ProfileButton, ProfileButtonText } from './styles';
+
 
 export default class Main extends Component{
     state = {
@@ -50,6 +52,22 @@ export default class Main extends Component{
             <Icon name="add" size={20} color="#FFF"/>
           </SubmitButton>
         </Form>
+
+        <List
+            data={users}
+            keyExtractor={user => user.login}
+            renderItem={({ item }) => (
+                <User>
+                    <Avatar source={{ uri: item.avatar }}/>
+                    <Name>{item.name}</Name>
+                    <Bio>{item.bio}</Bio>
+
+                    <ProfileButton onPress={() => {}}>
+                        <ProfileButtonText>Ver perfil</ProfileButtonText>
+                    </ProfileButton>
+                </User>
+            )}
+        />
       </Container>
     );
   }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -33,3 +33,56 @@ export const SubmitButton = styled(RectButton)`
     padding: 0 12px; /* padding nas laterais */
 
 `;
+
+export const List = styled.FlatList.attrs({
+    showsVerticalScrollIndicator: false,
+})`
+    margin-top: 20px;
+`;
+
+export const User = styled.View`
+    align-items: center;
+    margin: 0 20px 30px;
+`;
+
+export const Avatar = styled.Image`
+    width: 64px;
+    height: 64px;
+    border-radius: 32px; /* não temos porcentagem, então isso equivale ao border-radius 50% */
+    background: #eee; /* cor de fundo enquanto a imagem não carrega */
+`;
+
+export const Name = styled.Text`
+    font-size: 14px;
+    color: #333;
+    font-weight: bold;
+    margin-top: 4px;
+    text-align: center; /* caso o texto tenha mais de 1 linha, align-items não funfa */
+`;
+
+export const Bio = styled.Text.attrs({
+    numberOfLines: 2,
+})`
+    font-size: 13px;
+    line-height: 18px;
+    color: #999;
+    margin-top: 5px;
+    text-align: center;
+`;
+
+export const ProfileButton = styled(RectButton)`
+    margin-top: 10px;
+    align-self: stretch; /* alinha pra ocupar a largura total do componente. */
+    border-radius: 4px;
+    background: #e6b32a;
+    justify-content: center;
+    align-items: center;
+    height: 36px;
+`;
+
+export const ProfileButtonText = styled.Text`
+    font-size: 14px;
+    font-weight: bold;
+    color: #FFF;
+    text-transform: uppercase;
+`;
